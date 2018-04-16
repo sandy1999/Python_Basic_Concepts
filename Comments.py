36b2d9cb8b1ec7c435e13854f1b204b847d7e99b
Sanidhya Mangal <Sandyrockstar19@gmail.com>
2018-04-16 19:14:11 +0530

    First Commit include comments, maths and hello world

---
+++ Comments.py
@@ -0,0 +1,12 @@
+# A comment so that you can read your code later 
+# Anything after # is a comment 
+
+print("I could have code like this") # and comment after is ignored
+
+# You can also use a comment to "disabe"  a line or set of code
+
+# print("I wont run")
+
+print("I will run")
+
+print("Hi # there ") # in a code string will execute and get printed 
---
+++ HelloWorld.py
@@ -0,0 +1,6 @@
+#Author : Sanidhya Mangal <mangalsanidhya19@gmail.com> 
+
+print("Hello World ")
+print("This is my first script ")
+print("\n I love python\n")
+print("I am enjoying writing python")
