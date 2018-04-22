e9661847c893eb3c09b4e9805e2b868755f62923
Sanidhya Mangal <Sandyrockstar19@gmail.com>
2018-04-22 13:56:03 +0530

    Added: Some backslash terms

--- MorePrinting.py
+++ MorePrinting.py
@@ -32,3 +32,22 @@ print(formatter.format(
     "Maybe a poem",
     "Or a song about fear"
 ))
+
+
+# Using some tags or blackslash
+
+tabby_cat = "\t I'm tabbed in."
+persian_cat = "I'm split \non a line."
+backslash_cat = "I'm \\ a \\ cat"
+
+fat_cat = """
+I'll do a list:
+\t* Cat food 
+\t* Fishes
+\t* Catnip\n\t* Grass
+"""
+
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
