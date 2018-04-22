86491deaf42a7247238ae25106c73633a6438a04
Sanidhya Mangal <Sandyrockstar19@gmail.com>
2018-04-22 14:07:05 +0530

    Input:Added Prompting

--- Input.py
+++ Input.py
@@ -8,3 +8,11 @@ print("How much do you weigh ? ", end= ' ')
 weight = input()
 
 print(f"So you're {age} old, {height} tall and {weight} heavy.")
+
+# Prompting the people 
+
+age = int(input("How old are you? "))
+height = input("How tall are you? ")
+weight = float(input("How much do you weigh? "))
+
+print("So you're {} old, {} tall and {} heavy.".format(age,height,weight))
