8f2e0a3b451486b0f30b640ee55049dcc9e3f2ac
Sanidhya Mangal <Sandyrockstar19@gmail.com>
2018-04-22 14:03:32 +0530

    Added Input for python

---
+++ Input.py
@@ -0,0 +1,10 @@
+# This module is intented to get insights of how to take input in python
+
+print("How old are you ? ",end=" ")
+age = input()
+print("How tall are you ? ",end=' ')
+height = input()
+print("How much do you weigh ? ", end= ' ')
+weight = input()
+
+print(f"So you're {age} old, {height} tall and {weight} heavy.")
