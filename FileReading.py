b1b7a3b18638f0c6dc57678ff371da6359ba5f86
Sanidhya Mangal <Sandyrockstar19@gmail.com>
2018-05-26 18:53:03 +0530

    added: FileReading

---
+++ FileReading.py
@@ -0,0 +1,25 @@
+"""
+A script to read file as in text format
+"""
+
+from sys import argv
+#unpacking the variables as a arguments
+script,filename = argv
+
+#printing the file name that is been asked to read 
+print("Reading your file - {} :".format(filename))
+
+# opening the file as specified by the user.
+text = open(filename)
+
+#printing all the data that is present in our file 
+print(text.read())
+
+#Asking a user to enter a new file name 
+filename = input("Enter a name of new file or again\n >")
+
+# again opening the file 
+text_again = open(filename)
+
+#printing the contents of file 
+print(text_again.read())
