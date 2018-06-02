c4dfd89f13b1351f0c47b4eda19f2adf0b961de7
Sanidhya Mangal <Sandyrockstar19@gmail.com>
2018-06-02 19:54:15 +0530

    added: If Statements

---
+++ WhatIf.py
@@ -0,0 +1,41 @@
+""" 
+A py script dealing with if statements in py
+"""
+
+# declaring some variables 
+people = 20
+cats = 30
+dogs = 15
+
+# check if people less than cat
+if cats > people:
+    print("Too many cats! world is doomed")
+
+# check if people greater than cats 
+if people > cats:
+    print("Not many cats! World is saved")
+
+# check if people less than dogs 
+if people < dogs:
+    print("Worls is drooled on")
+
+# check if people are greater than dogs 
+if people > dogs:
+    print("The World is dry")
+
+# changing value of dogs 
+
+dogs += 5
+
+# check if people greater than equal dogs 
+
+if people >= dogs :
+    print("People are greater than or equal to dogs")
+
+# check if people less than equal to dogs 
+if people <= dogs:
+    print("People are less than equal to dogs")
+
+# check if people equal to dogs 
+if people == dogs:
+    print("People are equal to dogs")
